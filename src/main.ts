c3755af2450174b2fec152839db525d874520743
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2023-01-29 14:11:27 -0600

    modifies action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const githubToken = core.getInput('token');
+    const octokit = github.getOctokit(githubToken);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const context = github.context;
 
-    core.setOutput('time', new Date().toTimeString())
+    if (context.payload.pull_request) {
+      const { data: pullRequest } = await octokit.rest.pulls.get({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: context.payload.pull_request.number
+      });
+      core.debug(JSON.stringify(pullRequest))
+    }
+
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
